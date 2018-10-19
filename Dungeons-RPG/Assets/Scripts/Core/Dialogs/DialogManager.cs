ac0260ca8cf4e6b48d229b8a1692faf343b2726e
Alejandro <afernandezperez93@gmail.com>
2018-10-19 14:58:28 +0200

    Basic dialog manager

---
+++ Dungeons-RPG/Assets/Scripts/Core/Dialogs/DialogManager.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.Core.Dialogs
+{
+
+    public class DialogManager : MonoBehaviour
+    {
+        private DialogMessage currentMessage;
+        private string buffer;
+        private Text txtPlacer;
+        private DIALOG_MANAGER_STATE state = DIALOG_MANAGER_STATE.PAUSE;
+        private int letterIndex = 0;
+        private Queue<DialogMessage> messages = new Queue<DialogMessage>();
+        private float speed = 0.05f; //segundos por letra
+        private float time = 0.0f; 
+
+        public enum DIALOG_MANAGER_STATE
+        {
+            PAUSE = 0,
+            READING = 1
+        }
+
+        // Use this for initialization
+        void Start ()
+        {
+	    }
+	
+	    // Update is called once per frame
+	    void Update ()
+        {
+
+            if (state == DIALOG_MANAGER_STATE.READING)
+            {
+                //Muestra la letra en función de la velocidad
+                //Cuando se contabilice un tiempo igual a la velocidad de caracter por letra,
+                // se muestra la letra
+                if (this.time <= this.speed)
+                {
+                    this.time += Time.deltaTime;
+                }
+                else
+                {
+                    this.time = 0;
+
+                    if (letterIndex == this.currentMessage.message.Length)
+                    {
+                        this.nextMessage();
+                    }
+                    else
+                    {
+                        this.readCurrentMessage();
+                    }
+                }
+            }
+	    }
+
+        private void readCurrentMessage()
+        {
+
+            buffer += this.currentMessage.message[letterIndex];
+
+            letterIndex++;
+            txtPlacer.text = buffer;
+        }
+
+        private void nextMessage()
+        {
+            this.buffer = "";
+            this.letterIndex = 0;
+
+            if(this.messages.Count > 0)
+            {
+                this.currentMessage = messages.Dequeue();
+                Vector2 position = this.currentMessage.emitter.transform.position;
+                position.y += this.currentMessage.emitter.GetComponent<RectTransform>().rect.width;
+                this.gameObject.transform.SetPositionAndRotation(position,
+                                                                 this.currentMessage.emitter.transform.rotation);
+            }
+            else
+            {
+                this.stopReading();
+            }
+        }
+
+        public void addMessage(DialogMessage newMessage)
+        {
+            this.messages.Enqueue(newMessage);
+        }
+
+        public void startReading()
+        {
+            this.state = DIALOG_MANAGER_STATE.READING;
+            this.txtPlacer = gameObject.GetComponentsInChildren<Text>()[0];
+            this.nextMessage();
+        }
+
+        public void stopReading()
+        {
+            this.state = DIALOG_MANAGER_STATE.PAUSE;
+        }
+    }
+
+}
---
+++ Dungeons-RPG/Assets/Scripts/Core/Dialogs/TesterDialog.cs
@@ -0,0 +1,30 @@
+﻿using Assets.Scripts.Core.Dialogs;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TesterDialog : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        GameObject dialog = (GameObject)Instantiate(Resources.Load("Prefabs/Dialogs/DialogManager"));
+        dialog.transform.SetParent(this.gameObject.transform);
+
+        GameObject brutus = GameObject.Find("BrutusElPutus");
+
+        DialogManager dialogManager = (DialogManager) dialog.GetComponent<DialogManager>();
+
+        dialogManager.addMessage(new DialogMessage("Hola soy brutus", DialogMessage.MODE_NEXT_MESSAGE.AUTOMATIC, brutus));
+        dialogManager.addMessage(new DialogMessage("Tengo 2 añitos, crezco muy despacito ", DialogMessage.MODE_NEXT_MESSAGE.AUTOMATIC, brutus));
+        dialogManager.addMessage(new DialogMessage("soy brutus", DialogMessage.MODE_NEXT_MESSAGE.AUTOMATIC, brutus));
+
+        dialogManager.startReading();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
